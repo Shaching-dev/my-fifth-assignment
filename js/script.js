1100bd0cde1542cebffcc3b3e975d51559b41412
unknown <shaching.contact@gmail.com>
2025-08-28 03:35:56 +0600

    function commit

--- js/script.js
+++ js/script.js
@@ -409,3 +409,143 @@ const getClearButton = document
     const getHistoryContainers = document.getElementById("history-containers");
     getHistoryContainers.style.display = "none";
   });
+
+//   copy function----------------copy1
+let countCopy = 0;
+const getCopyButton = document
+  .getElementById("btn-copy")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const getCopyCounter = document.getElementById("copy-counter");
+    countCopy++;
+    getCopyCounter.textContent = countCopy;
+    const createCopiedTex = document.createElement("p");
+    createCopiedTex.innerText = "999";
+    navigator.clipboard.writeText(createCopiedTex.innerText);
+    window.alert("Number copied: 999");
+  });
+
+//   --------------------------2
+const getCopyButton1 = document
+  .getElementById("btn-copy-1")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const getCopyCounter = document.getElementById("copy-counter");
+    countCopy++;
+    getCopyCounter.textContent = countCopy;
+    const createCopiedTex = document.createElement("p");
+    createCopiedTex.innerText = "12145";
+    navigator.clipboard.writeText(createCopiedTex.innerText);
+    window.alert("Number copied: 12145");
+  });
+
+//   ----------------------------------3
+
+const getCopyButton2 = document
+  .getElementById("btn-copy-2")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const getCopyCounter = document.getElementById("copy-counter");
+    countCopy++;
+    getCopyCounter.textContent = countCopy;
+    const createCopiedTex = document.createElement("p");
+    createCopiedTex.innerText = "999";
+    navigator.clipboard.writeText(createCopiedTex.innerText);
+    window.alert("Number copied: 999");
+  });
+
+//----------------------------4
+const getCopyButton4 = document
+  .getElementById("btn-copy-3")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const getCopyCounter = document.getElementById("copy-counter");
+    countCopy++;
+    getCopyCounter.textContent = countCopy;
+    const createCopiedTex = document.createElement("p");
+    createCopiedTex.innerText = "456382";
+    navigator.clipboard.writeText(createCopiedTex.innerText);
+    window.alert("Number copied: 456382");
+  });
+//------------------------5
+
+const getCopyButton5 = document
+  .getElementById("btn-copy-4")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const getCopyCounter = document.getElementById("copy-counter");
+    countCopy++;
+    getCopyCounter.textContent = countCopy;
+    const createCopiedTex = document.createElement("p");
+    createCopiedTex.innerText = "10987";
+    navigator.clipboard.writeText(createCopiedTex.innerText);
+    window.alert("Number copied: 10987");
+  });
+
+//   -----------------------6
+
+const getCopyButton6 = document
+  .getElementById("btn-copy-5")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const getCopyCounter = document.getElementById("copy-counter");
+    countCopy++;
+    getCopyCounter.textContent = countCopy;
+    const createCopiedTex = document.createElement("p");
+    createCopiedTex.innerText = "43567";
+    navigator.clipboard.writeText(createCopiedTex.innerText);
+    window.alert("Number copied: 43567");
+  });
+
+//   ----------------7
+
+const getCopyButton7 = document
+  .getElementById("btn-copy-6")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const getCopyCounter = document.getElementById("copy-counter");
+    countCopy++;
+    getCopyCounter.textContent = countCopy;
+    const createCopiedTex = document.createElement("p");
+    createCopiedTex.innerText = "145678";
+    navigator.clipboard.writeText(createCopiedTex.innerText);
+    window.alert("Number copied: 145678");
+  });
+
+//   -----------------------8
+const getCopyButton8 = document
+  .getElementById("btn-copy-7")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const getCopyCounter = document.getElementById("copy-counter");
+    countCopy++;
+    getCopyCounter.textContent = countCopy;
+    const createCopiedTex = document.createElement("p");
+    createCopiedTex.innerText = "123456";
+    navigator.clipboard.writeText(createCopiedTex.innerText);
+    window.alert("Number copied: 123456");
+  });
+
+//   -----------------------9
+
+const getCopyButton9 = document
+  .getElementById("btn-copy-8")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const getCopyCounter = document.getElementById("copy-counter");
+    countCopy++;
+    getCopyCounter.textContent = countCopy;
+    const createCopiedTex = document.createElement("p");
+    createCopiedTex.innerText = "163";
+    navigator.clipboard.writeText(createCopiedTex.innerText);
+    window.alert("Number copied: 163");
+  });
